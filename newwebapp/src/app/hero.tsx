16306c150a37040c9edcbf380301eda8069d3223
M Sufyan Mughal <mskhalid703@gmail.com>
2024-08-31 22:50:48 +0500

    Update hero.tsx

--- newwebapp/src/app/hero.tsx
+++ newwebapp/src/app/hero.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { TypeAnimation } from 'react-type-animation';
 function Hero(){
     return (
-      <section id='hero'>
+      <section className='h-full' id='hero'>
       <div className="flex justify-between items-center p-2 ">
         <h1 className="text-1xl font-semibold font-mono md:text-3xl text-slate-400 ">HI THERE,
           <br />
